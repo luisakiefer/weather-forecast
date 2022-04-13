73961d878fe2090e40d814f3d6b743bbe131c194
Luísa Kiefer <91638544+luisakiefer@users.noreply.github.com>
2022-04-13 14:40:29 -0300

    updated js functions

--- src/index.js
+++ src/index.js
@@ -21,25 +21,34 @@ function currentDate() {
 let date = document.querySelector("#date");
 date.innerHTML = currentDate();
 
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  let h1 = document.querySelector("#city");
-  h1.innerHTML = searchInput.value;
+function displayWeather(response) {
+  document.querySelector("#city").innerHTLM = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector(
+    "#precipitation"
+  ).innerHTML = `${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.main.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
 
+function searchCity(city) {
   let apiKey = "dfb7d1f03e3eb9184605492c12826c7d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
 
-  function displayTemp(response) {
-    let temp = Math.round(response.data.main.temp);
-    let h2 = document.querySelector("h2");
-    h2.innerHTML = `${temp}°C`;
-  }
-
-  axios.get(apiUrl).then(displayTemp);
+function submitSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
 }
 
 let form = document.querySelector("#search-city-form");
-form.addEventListener("submit", search);
+form.addEventListener("submit", submitSearch);
 
 let tempElement = document.querySelector("#temperature");
