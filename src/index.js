8f07c99229ee3cee8946b7c83b8b24ff769da270
Luísa Kiefer <91638544+luisakiefer@users.noreply.github.com>
2022-04-17 10:47:40 -0300

    trying to fix js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ let date = document.querySelector("#date");
 date.innerHTML = currentDate();
 
 function displayWeather(response) {
-  let city = document.querySelector("#city-name");
+  let city = document.querySelector("#city");
   city.innerHTLM = response.data.name;
 
   let currentTemp = document.querySelector("#temperature");
